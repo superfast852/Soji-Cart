1369f92d7445f445a2ed948945696198a39f372d
Gabriel Garcia <superfast852@gmail.com>
2023-03-07 00:04:38 -0400

    New Arm Test

--- arm_test.py
+++ arm_test.py
@@ -1,4 +1,6 @@
 from adafruit_servokit import ServoKit
+from utilities.comms import Server
+from socket import gethostbyname
 
 kit = ServoKit(channels=8)
 arm = [kit.servo[i] for i in range(6)]
@@ -6,5 +8,19 @@ arm = [kit.servo[i] for i in range(6)]
 # Set the arm to the home position
 home = [90, 45, 180, 45, 0, 125]
 
-for i, angle in enumerate(home):
-    arm[i].angle = angle
+s = Server(gethostbyname("soji.local"), 9160)
+
+while True:
+    try:
+        conn, addr = s.connect()
+        print(f"CONNECTED: {addr}")
+        while conn:
+            try:
+                pose = [int(i) for i in conn.recv(1024).split(", ")]
+                for i, angle in enumerate(pose):
+                    arm[i].angle = angle
+            except Exception as e:
+                print(f"[ERROR] {addr}: {e}")
+    except Exception as e:
+        print(f"[ERROR]: {e}")
+        continue
