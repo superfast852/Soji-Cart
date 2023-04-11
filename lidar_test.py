1253ad1c724771d403cc8fb62789956dfd1d0474
superfast852 <superfast852@gmail.com>
2023-04-11 19:53:12 -0400

    Added lidar fixes

--- lidar_test.py
+++ lidar_test.py
@@ -1,13 +1,13 @@
 from rplidar import RPLidar
 from utilities.pi import Drive
 from utilities.collision import check_surroundings, check_collision
-from utilities.utils import plot
+from utilities.utils import plot, plt
 
 collision_space = 20  # Range of angles to check for obstacles in front of car
-collision_threshold = 5  # Minimum distance for the code to consider as obstacle.
+collision_threshold = 250  # Minimum distance for the code to consider as obstacle.
 spin_intensity = 4  # Divides max_speed by this to spin robot in setCourse()
 max_speed = 100
-collision_bounds = (270 - (collision_space // 2), 270 + (collision_space // 2))
+collision_bounds = (200, 300)
 motors = Drive(16, 12, 21, 20)
 lidar = RPLidar('/dev/ttyUSB0', timeout=3)
 motors.brake()
@@ -18,7 +18,7 @@ def set_course(lidar_data):
 
         # If there is an obstacle...
         direction, angles = check_surroundings(lidar_data, collision_threshold)  # Find out the clearest side
-        if direction == "Left":
+        if direction == "Right":
             motors.setLeftSpeed(max_speed/spin_intensity)
             motors.setRightSpeed(max_speed)
         else:
@@ -36,6 +36,5 @@ scan = [0]*360
 for data in lidar.iter_scans():
     for _, angle, distance in data:
         scan[min(359, int(angle))] = distance/10
-
-    set_course(scan)
-    plot(scan)
+    print(min(scan), set_course(scan))
+    #plot(scan)
